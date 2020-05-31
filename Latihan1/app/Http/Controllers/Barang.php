22a18056470dbd538d0d103d70ea1ce7064a968d
Muhamar <amararrasyid@gmail.com>
2020-05-31 23:27:34 +0700

    Crud API Laravel

---
+++ Latihan1/app/Http/Controllers/Barang.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\tbl_katalog;
+class Barang extends Controller
+{
+    public function getData() {
+        $data = DB::table('tbl_katalog')->get();
+        if(count($data) > 0){
+            $res['message'] = "Success";
+            $res['value'] = $data;
+            return response($res);
+        }else{
+            $res['message'] = "Empty!";
+            return response($res);
+        }
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'file' => 'required|max:2048'
+        ]);
+
+        //Mentyimpan data file yang diupload ke variabel $_FILES
+        $file = $request->file('file');
+        $nama_file = time()."_".$file->getClientOriginalName();
+
+        //isi dengan nama folder tempat kemana file diupload
+        $tujuan_upload = 'data_file';
+
+        if($file->move($tujuan_upload, $nama_file)){
+            $data = tbl_katalog::create([
+                'nama_produk' => $request->nama_produk,
+                'berat'=> $request->berat,
+                'harga'=> $request->harga,
+                'gambar' => $nama_file,
+                'keterangan'=> $request->keterangan
+            ]);
+            $res['message'] = "Success!";
+            $res['value'] = $data;
+            return response($res);
+        }
+    }
+
+    public function update(Request $request){
+        if(!empty($request->file)){
+            $this->validate($request, [
+                'file' => 'required|max:2048'
+            ]);
+
+            $file = $request->file('file');
+
+            $nama_file = time()."_".$file->getClientOriginalName();
+
+            $tujuan_upload = 'data_file';
+            $file->move($tujuan_upload, $nama_file);
+            $data = DB::table('tbl_katalog')->where('id', $request->id)->get();
+            foreach($data as $katalog){
+                @unlink(public_path('data_file/'.$katalog->gambar));
+                $ket = DB::table('tbl_katalog')->where('id', $request->id)->update([
+                    'nama_produk' =>$request->nama_produk,
+                    'berat' => $request->berat,
+                    'harga' => $request->harga,
+                    'gambar' => $nama_file,
+                    'keterangan' => $request->keterangan
+                ]);
+                $res['message'] = "Success!";
+                $res['value'] = $ket;
+                return response($res);
+            }
+        }else{
+            $data = DB::table('tbl_katalog')->where('id', $request->id)->get();
+            foreach($data as $katalog){
+                $ket = DB::table('tbl_katalog')->where('id', $request->id)->update([
+                    'nama_produk' =>$request->nama_produk,
+                    'berat' => $request->berat,
+                    'harga' => $request->harga,
+                    'keterangan' => $request->keterangan
+                ]);
+                $res['message'] = "Success!";
+                $res['value'] = $ket;
+                return response($res);
+            }
+        }
+    }
+
+    public function hapus($id){
+        $data = DB::table('tbl_katalog')->where('id',$id)->get();
+        foreach($data as  $katalog){
+            if(file_exists(public_path('data_file/'.$katalog->gambar))) {
+                @unlink(public_path('data_file/'.$katalog->gambar));
+                DB::table('tbl_katalog')->where('id', $id)->delete();
+                $res['message'] = "Success!";
+                return response($res);
+            }else{
+                $res['message'] = "Empty!";
+                return response($res);
+            }
+        }
+    }
+
+    public function getDetail($id){
+        $data = DB::table('tbl_katalog')->where('id', $id)->get();
+        if(count($data) > 0){
+            $res['message'] = "Success!";
+            $res['value'] = $data;
+            return response($res);
+        }else{
+            $res['messagge'] = "Empty";
+            return response($res);
+        }
+    }
+}
